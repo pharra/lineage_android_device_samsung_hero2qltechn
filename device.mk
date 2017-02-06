3b152d7c054994159c301d2cbe19a466ec17afdc
travis mills <travismills82@gmail.com>
2017-02-06 10:52:54 +0800

    remove android_filesystem_config

--- device.mk
+++ device.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
-# For android_filesystem_config.h
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8996 \
