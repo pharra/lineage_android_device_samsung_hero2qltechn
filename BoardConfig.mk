66307ce15f04c79ce98c38584e8e92dc65cda3a8
Travis Mills <travismills82@gmail.com>
2017-02-16 20:08:55 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := lineage_hero2qltechn_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8996
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8996
@@ -182,6 +183,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+RECOVERY_SDCARD_ON_DATA := true
 
 # Ramdisk
 BOARD_ROOT_EXTRA_FOLDERS := dsp efs firmware firmware-modem persist
